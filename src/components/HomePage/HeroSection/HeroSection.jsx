f75b1fc2b926d0c0008d673a0d499be35a2db571
AJIT-KUMAR-PANDIT <ajitkumarpandit1999@gmail.com>
2025-10-11 16:57:07 +0530

    update

--- src/components/HomePage/HeroSection/HeroSection.jsx
+++ src/components/HomePage/HeroSection/HeroSection.jsx
@@ -1,37 +1,28 @@
 import React from "react";
 import styles from "./HeroSection.module.scss";
-import Image from "next/image";
 import Typewriter from "@/components/ui/TypeWriter";
+import clsx from "clsx";
 
 const HeroSection = () => {
   return (
-    <>
-      <section
-        className={styles["hero-section"] + " lg:" + styles["hero-section-pc"]}
-      >
-        <section className={styles["hero-left-content"]}>
-          <h1 className={styles["hero-title"] + " myFont"}>
-            <Typewriter
-              texts={[
-                "Hi",
-                "I am",
-                "Ajit Kumar Pandit",
-                "A Fullstack Developer",
-              ]}
-              typingSpeed={150}
-              pauseDuration={1000}
-            />
-          </h1>
-          <p className={styles["hero-subtitle"]}>Frontend Developer</p>
-        </section>
-        <section className={styles["hero-right-content"]}>
-          <video autoPlay loop muted className={styles["background-video"]}>
-            <source src="/assets/loder-video.webm" type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
-        </section>
-      </section>
-    </>
+    <section className={clsx(styles.heroSection)}>
+      <div className={styles["hero-left"]}>
+        <h1 className={clsx(styles["hero-title"], "myFont")}>
+          <Typewriter
+            texts={["Hi", "I am", "Ajit Kumar Pandit", "A Fullstack Developer"]}
+            typingSpeed={150}
+            pauseDuration={1000}
+          />
+        </h1>
+        <p className={styles["hero-subtitle"]}>Frontend Developer</p>
+      </div>
+      <div className={styles["hero-right"]}>
+        <video autoPlay loop muted className={styles["background-video"]}>
+          <source src="/assets/loder-video.webm" type="video/webm" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+    </section>
   );
 };
 
