871da3dcc7f471efef434ab6fdd7c42ace288965
AJIT-KUMAR-PANDIT <ajitkumarpandit1999@gmail.com>
2025-10-11 17:55:36 +0530

    upadte

--- src/components/HomePage/HeroSection/HeroSection.jsx
+++ src/components/HomePage/HeroSection/HeroSection.jsx
@@ -6,7 +6,7 @@ import clsx from "clsx";
 const HeroSection = () => {
   return (
     <section className={clsx(styles["hero-section"])}>
-      <div className={styles["hero-left"]}>
+      <div className={clsx(styles["hero-left"])}>
         <h1 className={clsx(styles["hero-title"], "myFont")}>
           <Typewriter
             texts={["Hi", "I am", "Ajit Kumar Pandit", "A Fullstack Developer"]}
@@ -14,12 +14,19 @@ const HeroSection = () => {
             pauseDuration={1000}
           />
         </h1>
-        <p className={styles["hero-tagline"]}>
+        <h5 className={clsx(styles["hero-tagline"])}>
           eNHANCING fUTURE wITH tECHNOLOGY
-        </p>
+        </h5>
+        <h6 className={clsx(styles["hero-text1"])}>
+          Building Digital Experiences|
+        </h6>
+        <div className={clsx(styles["hero-btn"])}>
+          <button className={clsx("btn-primary")}>Contact Me</button>
+          <button className={clsx("btn-secondary")}>View My Work</button>
+        </div>
       </div>
-      <div className={styles["hero-right"]}>
-        <video autoPlay loop muted className={styles["background-video"]}>
+      <div className={clsx(styles["hero-right"])}>
+        <video autoPlay loop muted className={clsx(styles["background-video"])}>
           <source src="/assets/loder-video.webm" type="video/webm" />
           Your browser does not support the video tag.
         </video>
