88248fd5506028b417e828b54dfae4cfac63b84f
AJIT-KUMAR-PANDIT <ajitkumarpandit1999@gmail.com>
2025-10-14 14:32:35 +0530

    up

--- src/components/common/ClientLayout/ClientLayout.jsx
+++ src/components/common/ClientLayout/ClientLayout.jsx
@@ -4,7 +4,6 @@ import React, { useState, useEffect } from "react";
 import Loader from "@/components/common/Loader/Loader";
 import Dock from "@/components/common/Dock/Dock";
 import { Nav } from "@/components/common/Nav/Nav";
-import Lenis from "lenis";
 
 export default function ClientLayout({ children }) {
   const [loading, setLoading] = useState(true);
@@ -13,19 +12,6 @@ export default function ClientLayout({ children }) {
     setTimeout(() => {
       setLoading(false);
     }, 2000); // Simulate a 2-second loading time
-
-    const lenis = new Lenis();
-
-    function raf(time) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    }
-
-    requestAnimationFrame(raf);
-
-    return () => {
-      lenis.destroy();
-    };
   }, []);
 
   return (
