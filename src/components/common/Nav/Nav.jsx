b8391e0e8cb0af330882248bdc7e4ffffc7afff6
AJIT-KUMAR-PANDIT <ajitkumarpandit1999@gmail.com>
2025-10-14 13:16:51 +0530

    up

--- src/components/common/Nav/Nav.jsx
+++ src/components/common/Nav/Nav.jsx
@@ -6,7 +6,7 @@ export const Nav = () => {
   return (
     <>
       <nav className="z-[999999999] fixed top-0 left-0 right-0 flex items-center sm:justify-center md:justify-center px-10 w-[100vw] ">
-        <Link href="/">
+        <Link href="/" className="backdrop-blur-md">
           <Image
             src="/ajitkumarpandit/AJITKUMARPANDIT_LOGO.png"
             alt="logo"
