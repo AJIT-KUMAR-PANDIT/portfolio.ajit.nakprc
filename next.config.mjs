c4446ffa61bd7bcda0be1f339f6bb76894a832b8
AJIT-KUMAR-PANDIT <ajitkumarpandit1999@gmail.com>
2025-10-15 19:09:12 +0530

    upadte

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactCompiler: true,
+  async redirects() {
+    return [
+      {
+        source: '/admin',
+        destination: 'https://admin.ajitkumarpandit.nakprc.com',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
