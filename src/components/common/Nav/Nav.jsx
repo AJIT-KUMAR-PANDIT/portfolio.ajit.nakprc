fa7b06d0316a72b31250e494f086f6a0b03ed8f1
AJIT-KUMAR-PANDIT <ajitkumarpandit1999@gmail.com>
2026-03-15 17:22:51 +0530

    kjsdln

--- src/components/common/Nav/Nav.jsx
+++ src/components/common/Nav/Nav.jsx
@@ -5,13 +5,14 @@ import Link from "next/link";
 export const Nav = () => {
   return (
     <>
-      <nav className="z-[999999999] fixed top-0 left-0 right-0 flex items-center sm:justify-center md:justify-center px-10 w-[100vw] ">
-        <Link href="/" className="backdrop-blur-md">
+      <nav className="z-[999999999] fixed top-0 left-0 right-0 flex items-center justify-center w-[100vw] ">
+        <Link href="/" className="backdrop-blur-md w-[100vw] bg-black-50">
           <Image
             src="/ajitkumarpandit/AJITKUMARPANDIT_LOGO.png"
             alt="logo"
             width={200}
             height={100}
+            className="w-[200px] h-[79px] object-cover"
           />
         </Link>
       </nav>
