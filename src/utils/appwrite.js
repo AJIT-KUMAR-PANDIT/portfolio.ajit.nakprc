fee6dae290fe20a45d79ea12f88308c4ec45a13b
AJIT-KUMAR-PANDIT <ajitkumarpandit1999@gmail.com>
2026-07-13 22:39:40 +0530

    x

--- src/utils/appwrite.js
+++ src/utils/appwrite.js
@@ -4,7 +4,13 @@ export const client = new Client();
 
 import { APPWRITE_ENDPOINT, APPWRITE_PROJECT_ID } from "@/utils/env";
 
-client.setEndpoint(APPWRITE_ENDPOINT).setProject(APPWRITE_PROJECT_ID);
+if (APPWRITE_ENDPOINT) {
+  client.setEndpoint(APPWRITE_ENDPOINT);
+}
+
+if (APPWRITE_PROJECT_ID) {
+  client.setProject(APPWRITE_PROJECT_ID);
+}
 
 export const account = new Account(client);
 export const tablesDB = new TablesDB(client);
